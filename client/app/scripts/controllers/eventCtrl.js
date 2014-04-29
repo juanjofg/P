46416a9b6481747df476d6d20e37e1d9e5737e1b
juanjofg <juanjo@home.rest>
2014-04-29 23:06:26 +0200

    Generating cells and rows from ng-repeat

--- client/app/scripts/controllers/eventCtrl.js
+++ client/app/scripts/controllers/eventCtrl.js
@@ -4,7 +4,15 @@ angular.module('PintxApp')
   .controller('EventCtrl', ['$scope','$routeParams', 'Events',
     function($scope, $routeParams, Events){
       function drawEvents (res){
-        $scope.locals = res[0].locals;
+        //$scope.locals = res[0].locals;
+        var locals = [];
+        for (var i = 0; i < res[0].locals.length; i++){
+          if (i % 4 === 0){
+            locals.push([]);
+          }
+          locals[locals.length-1].push(res[0].locals[i]);
+        }
+        $scope.locals = locals;
       }
       function errorHandler (err) {
         console.log(err);
