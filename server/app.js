7290c9a0246e3afaed009ff59be6fbd981c2532f
juanjofg <juanjo@home.rest>
2014-05-05 22:36:53 +0200

    Added options to MongoDB connection string

--- server/app.js
+++ server/app.js
@@ -7,11 +7,23 @@ var app         = express();
 var bodyParser  = require('body-parser');
 var path        = require('path');
 var mongoose    = require('mongoose');
+var MONGO = {
+  options: {
+    server: {
+      auto_reconnect: true,
+      socketOptions: {
+        connectTimeoutMS: 3600000,
+        keepAlive: 3600000,
+        socketTimetoutMS: 3600000
+      }
+    }
+  }
+};
 // Configure server to use bodyParser
 // to get data from POST request
 app.use(bodyParser());
 
-mongoose.connect('mongodb://localhost/events');
+//mongoose.connect('mongodb://localhost/events', MONGO.options);
 
 var port = process.env.PORT || 3000;
 
@@ -22,7 +34,7 @@ if ('development' == app.get('env')){
   app.use(express.static(path.join(__dirname,'../client/app')));
 }
 if ('production' == app.get('env')){
-  app.use(express.static(__dirname + '/client/dist'));
+  app.use(express.static(__dirname + '/server/dist'));
 }
 
 // ROUTES
