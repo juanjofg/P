70cbf2f3c36984f00e181ac444dd455183275ac1
juanjofg <juanjo@home.rest>
2014-05-06 22:59:22 +0200

    Check array length

--- client/app/scripts/controllers/eventCtrl.js
+++ client/app/scripts/controllers/eventCtrl.js
@@ -4,14 +4,16 @@ angular.module('PintxApp')
   .controller('EventCtrl', ['$scope','$routeParams', 'Events',
     function($scope, $routeParams, Events){
       function drawEvents (res){
-        var locals = [];
-        for (var i = 0; i < res[0].locals.length; i++){
-          if (i % 4 === 0){
-            locals.push([]);
+        if (res.length){
+          var locals = [];
+          for (var i = 0; i < res[0].locals.length; i++){
+            if (i % 4 === 0){
+              locals.push([]);
+            }
+            locals[locals.length-1].push(res[0].locals[i]);
           }
-          locals[locals.length-1].push(res[0].locals[i]);
+          $scope.locals = locals;
         }
-        $scope.locals = locals;
       }
       function errorHandler (err) {
         console.log(err);
