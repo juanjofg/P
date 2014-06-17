1a8962b657dca4e9ed86ba9acd71220bb9689d3c
juanjofg <juanjo@home.rest>
2014-06-17 23:06:26 +0200

    Updated event model

--- server/models/events.js
+++ server/models/events.js
@@ -1,8 +1,29 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
+var Local = new Schema({
+  id: Number,
+  name: String,
+  snack: String,
+  address: String,
+  loc: {
+    lon: Number,
+    lat: Number
+  },
+  schedule: String,
+  additional: String,
+  phone: String,
+  visits: Number
+});
+
 var EventSchema   = new Schema({
-  name: String
+  name: String,
+  region: String,
+  location: String,
+  date: Date,
+  info: String,
+  price: String,
+  locals: [Local]
 });
 
 module.exports = mongoose.model('Event', EventSchema);
