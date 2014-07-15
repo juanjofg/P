659fb0d5d04853032898b8d0fb64f2add9ef7c45
Juanjo <juanj.fdez@gmail.com>
2014-07-16 00:03:01 +0200

    Added getDistance method to this factory service

--- client/app/scripts/services/geolocationSrv.js
+++ client/app/scripts/services/geolocationSrv.js
@@ -10,6 +10,10 @@ angular.module('PintxApp')
       geolocation = window.navigator.geolocation;
     }
 
+    function deg2grad (deg) {
+      return deg * (Math.PI/180);
+    }
+
     return {
       getPosition: function(success, error){
         if (geolocation){
@@ -25,6 +29,20 @@ angular.module('PintxApp')
         if (geolocation){
           geolocation.clearWatch(identifier);
         }
+      },
+      /* Calculate distance
+      * http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
+      */
+      getDistance: function (lat1, lon1, lat2, lon2) {
+        var R = 6371, // Radius of the earth in km
+          dLat = deg2grad(lat2 - lat1),
+          dLon = deg2grad(lon2 - lon1),
+          a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+              Math.cos(deg2grad(lat1)) * Math.cos(deg2grad(lat2)) *
+              Math.sin(dLon/2) * Math.sin(dLon/2),
+          c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)),
+          d = R * c; //Distance in km
+          return d;
       }
     };
   }]);
